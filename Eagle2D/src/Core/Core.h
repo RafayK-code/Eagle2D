e75fac72948127e0e561265abf4ff343e3d56e48
kashifrafay075@gmail.com <kashifrafay075@gmail.com>
2021-03-04 18:53:33 -0500

    completely overriding previous Eagle2D

--- Eagle2D/src/Core/Core.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef EAGLE_PLATFORM_WINDOWS
-	#ifdef EAGLE_BUILD_DLL
-		#define EAGLE_API __declspec(dllexport)
-	#else 
-		#define EAGLE_API __declspec(dllimport)
-	#endif
-#endif
--- Eagle2D/src/Eagle2D.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-//For use in applications
-#include "EaglePCH.h"
-
-#include "Graphics/RendererWindow.h"
-#include "Core/Event.h"
